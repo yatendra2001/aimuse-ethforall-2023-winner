8e4421a2a75751b3824ed42d07ac1efb074fd47f
Yatendra Kumar <yatendraroria2001@gmail.com>
2023-02-08 13:05:38 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ai_muse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ai_muse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ai_muse/pages/pages.dart';
+import 'package:ai_muse/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        // MyRoutes.loginRoute: (context) => const LoginPage(),
+      },
+    );
+  }
+}
